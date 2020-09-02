6f549fe5bae15b068a11d0472b381e8b6a3e4f38
= <=>
2020-09-02 18:15:38 +0300

    #1 Direction: fix: naming of MaxPlaces to MaxSeats field

--- models/Direction.cs
+++ models/Direction.cs
@@ -9,7 +9,7 @@ namespace air_ticket_cashier
         public String FromCountry { get; set; }
         public String ToCountry { get; set; }
         public bool Avaliable { get; set; }
-        public int MaxPlaces { get; set; }
+        public int MaxSeats { get; set; }
         public String PlaneCode { get; set; }
         public DirectionType DirectionType { get; set; }
 
