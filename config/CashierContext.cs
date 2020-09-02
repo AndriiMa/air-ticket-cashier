c45dc4ddf635953ba669c02c21badc0d9b4f7f5b
= <=>
2020-09-02 18:13:36 +0300

    #1 CashierContext: fix: naming of tables

--- config/CashierContext.cs
+++ config/CashierContext.cs
@@ -6,14 +6,13 @@ namespace air_ticket_cashier
 
     public class CashierContext : DbContext
     {
-
         public DbSet<Direction> Directions { get; set; }
         public DbSet<Flight> Flights { get; set; }
 
          protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Direction>().ToTable("");
-            modelBuilder.Entity<Flight>().ToTable("Schedule");
+            modelBuilder.Entity<Direction>().ToTable("direction");
+            modelBuilder.Entity<Flight>().ToTable("flight");
         }
     }
 }
