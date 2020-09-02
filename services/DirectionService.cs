c7d86e7a06d1190a92c2300963f7e447012d7d26
= <=>
2020-09-02 18:17:09 +0300

    #1 DirectionService: enhance: CashierContext now created by framework container

--- services/DirectionService.cs
+++ services/DirectionService.cs
@@ -5,28 +5,30 @@ namespace air_ticket_cashier
 {
     public class DirectionService
     {
+        private CashierContext context;
+
+        public DirectionService(CashierContext cashierContext)
+        {
+            this.context = cashierContext;
+        }
+
         public Direction GetById(int id)
         {
+
             Direction direction = null;
-            using (var context = new CashierContext())
-            {
-                direction = context.Directions
-                                     .Where(s => s.ID == id)
-                                     .FirstOrDefault<Direction>();
-                return direction;
-            }
+            direction = context.Directions
+                                 .Where(s => s.ID == id)
+                                 .FirstOrDefault<Direction>();
+            return direction;
         }
 
         public List<Direction> GetAvailable()
         {
             List<Direction> directions = null;
-            using (var context = new CashierContext())
-            {
-                directions = context.Directions
-                                     .Where(s => s.Avaliable == true)
-                                     .ToList();
-                return directions;
-            }
+            directions = context.Directions
+                                 .Where(s => s.Avaliable == true)
+                                 .ToList();
+            return directions;
         }
     }
 }
