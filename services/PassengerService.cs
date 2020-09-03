6404b745aec6dc7e8a75df5efc768a49c9f3540f
= <=>
2020-09-03 23:48:03 +0300

    #2 PassengerService: refactor: update and delete logic by firstly getting entity from bd

--- services/PassengerService.cs
+++ services/PassengerService.cs
@@ -25,14 +25,22 @@ namespace air_ticket_cashier
             return passenger;
         }
 
-        public void Update(Passenger passenger)
+        public void Update(Passenger passengerUpdates)
         {
+            Passenger passenger = context.Passengers.FirstOrDefault(p => p.ID == passengerUpdates.ID);
+            passenger.FirstName = passengerUpdates.LastName;
+            passenger.LastName = passengerUpdates.LastName;
+            passenger.PassengerSex = passengerUpdates.PassengerSex;
+            
+
             context.Passengers.Update(passenger);
             context.SaveChanges();
         }
 
-        public void Delete(Passenger passenger)
+        public void DeleteById(int id)
         {
+            Passenger passenger = context.Passengers.FirstOrDefault(p => p.ID == id);
+
             context.Passengers.Remove(passenger);
             context.SaveChanges();
         }
