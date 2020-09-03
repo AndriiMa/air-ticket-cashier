7b2b6df5a2c8626ed1dd71bccbb6e94fe53f6bcd
= <=>
2020-09-03 23:10:22 +0300

    #2 FlightController: add: Flight model controller

---
+++ controllers/FlightController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace air_ticket_cashier
+{
+    [ApiController]
+    [Route("/api/flights")]
+    public class FlightController
+    {
+
+        private FlightService flightService;
+
+        public FlightController(FlightService flightService)
+        {
+            this.flightService = flightService;
+        }
+
+        [HttpGet("")]
+        public List<Flight> GetAvailableFlights()
+        {
+            return flightService.GetAvailable();
+        }
+
+        [HttpGet("/{id}")]
+        public Flight GetFlightById(int id)
+        {
+            return flightService.GetById(id);
+        }
+
+        [HttpGet("")]
+        public List<Flight> GetFlightByDeparture(DepartureDto departureDto)
+        {
+            return flightService.GetByDeparture(departureDto.Departure);
+        }
+    }
+}
---
+++ controllers/dto/DepartureDto.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace air_ticket_cashier
+{
+    public class DepartureDto
+    {
+        public DateTime Departure { get; set; }
+    }
+}
