1e278174e385558d91d96de3b4ad942ee67f3a49
= <=>
2020-09-03 16:25:38 +0300

    #1 Flight: fix: naming of Avaliable field

--- models/Flight.cs
+++ models/Flight.cs
@@ -8,7 +8,7 @@ namespace air_ticket_cashier
         public int ID { get; set; }
         public DateTime DeparureAt { get; set; }
         public DateTime ArriveAt { get; set; }
-        public bool Available { get; set; }
+        public bool Avaliable { get; set; }
         public Direction Direction { get; set; }
 
     }
