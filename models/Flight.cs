77a5de98283c18ee16865c51f0ab603461f0dddd
= <=>
2020-09-07 12:52:50 +0300

    Flight: refactor: rename DepartureAt field

--- models/Flight.cs
+++ models/Flight.cs
@@ -6,7 +6,7 @@ namespace air_ticket_cashier
     public class Flight
     {
         public int ID { get; set; }
-        public DateTime DeparureAt { get; set; }
+        public DateTime DepartureAt { get; set; }
         public DateTime ArriveAt { get; set; }
         public bool Avaliable { get; set; }
         public Direction Direction { get; set; }
